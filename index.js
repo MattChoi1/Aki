65875ec502384c0923073904842e9d626e5e11ab
Matt Choi <kjw9411@berkeley.edu>
2017-07-25 05:48:28 -0700

    initial commit with vue setup

---
+++ index.js
@@ -0,0 +1,10 @@
+require('./endpoints.js');
+
+var http = require('http');
+
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
